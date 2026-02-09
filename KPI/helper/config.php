ad79d84bf47f8852812c6624aeef7d7f51e22c40
Anang Prayoga <Prayoga@karisma>
2026-02-09 10:14:01 +0700

    online

--- KPI/helper/config.php
+++ KPI/helper/config.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "db_kpi";
+$server = "https://hrd.karismaonline.id/kpi/";
+$user = "u471548307_adminkarisma";
+$pass = "?BQlb>[>6";
+$database = "u471548307_karismaerp";
 $conn = mysqli_connect($server, $user, $pass, $database);
 if (!$conn) {
     header("Location: error");
